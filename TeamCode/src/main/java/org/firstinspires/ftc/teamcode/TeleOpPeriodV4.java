b39f5c15d5673a4d295b8ac6aee29bf8973c7b7e
ArjanB2001 <arjanblankestijn@gmail.com>
2018-01-24 11:49:58 +0100

    heel veel veranderingen met rijden

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpPeriodV4.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpPeriodV4.java
@@ -4,8 +4,8 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name = "TeleOpPeriodV3", group = "Linear Opmode")//
-public class TeleOpPeriodV3 extends LinearOpMode {
+@TeleOp(name = "TeleOpPeriodV4 ", group = "Linear Opmode")//
+public class TeleOpPeriodV4 extends LinearOpMode {
 
     //om HardwareVar class te kunnen gebruiken (gebruik voor elke variabele r.)
     HardwareVar r = new HardwareVar();
@@ -21,7 +21,7 @@ public class TeleOpPeriodV3 extends LinearOpMode {
         waitForStart();
  
         while(opModeIsActive()){
-            drive    = Range.clip(gamepad1.right_trigger - gamepad1.left_trigger, -0.8, 0.8);
+            drive    = Range.clip(gamepad1.right_trigger - gamepad1.left_trigger, -0.3, 0.3);
 
             if (drive > 0 && !gamepad1.dpad_right && !gamepad1.dpad_left) {
                 r.LFpower = drive;
@@ -42,17 +42,17 @@ public class TeleOpPeriodV3 extends LinearOpMode {
 
 
             if (gamepad1.dpad_left && drive == 0) {
-                r.LFpower = -1;
-                r.LBpower = -1;
-                r.RBpower =  1;
-                r.RFpower =  1;
+                r.LFpower = -0.8;
+                r.LBpower = -0.8;
+                r.RBpower =  0.8;
+                r.RFpower =  0.8;
             }
 
             if (gamepad1.dpad_right && drive == 0) {
-                r.LFpower =  1;
-                r.LBpower =  1;
-                r.RBpower =  -1;
-                r.RFpower =  -1;
+                r.LFpower =  0.8;
+                r.LBpower =  0.8;
+                r.RBpower =  -0.8;
+                r.RFpower =  -0.8;
                 ;
             }
 
@@ -95,8 +95,8 @@ public class TeleOpPeriodV3 extends LinearOpMode {
             // = Range.clip(gamepad1.right_stick_x, 0  , 1.0) ;
 
             if(gamepad1.a) {
-                r.servo1.setPosition(0.3);
-                telemetry.addData("Servo:", "0.3");
+                r.servo1.setPosition(0.4);
+                telemetry.addData("Servo:", "0.4");
             }
 
             if(gamepad1.y) {
@@ -110,8 +110,8 @@ public class TeleOpPeriodV3 extends LinearOpMode {
             }
 
             if(gamepad1.x) {
-                r.servo1.setPosition(0.3);
-                telemetry.addData("Servo:", "0.2");
+                r.servo1.setPosition(0.6);
+                telemetry.addData("Servo:", "0.6");
             }
 
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpPeriodV5.java
@@ -0,0 +1,69 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name = "TeleOpPeriodV5 ", group = "Linear Opmode")//
+public class TeleOpPeriodV5 extends LinearOpMode {
+
+    //om HardwareVar class te kunnen gebruiken (gebruik voor elke variabele r.)
+    HardwareVar r = new HardwareVar();
+    double degree;
+    double drive;
+
+    public void runOpMode(){
+        //r. runt HardwareVar class
+        r.init(hardwareMap);
+        //reset de timer
+        r.runtime.reset();
+        //wacht tot play (niet init) ingedrukt wordt
+        waitForStart();
+ 
+        while(opModeIsActive()){
+            double o = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
+            double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+            double rightX = gamepad1.right_stick_x;
+            final double v1 = o * Math.cos(robotAngle) + rightX;
+            final double v2 = o * Math.sin(robotAngle) - rightX;
+            final double v3 = o * Math.sin(robotAngle) + rightX;
+            final double v4 = o * Math.cos(robotAngle) - rightX;
+
+            r.LFmotor.setPower(v1);
+            r.RFmotor.setPower(v2);
+            r.LBmotor.setPower(v3);
+            r.RBmotor.setPower(v4);
+
+            if(gamepad1.a) {
+                r.servo1.setPosition(0.4);
+                telemetry.addData("Servo:", "0.4");
+            }
+
+            if(gamepad1.y) {
+                r.servo1.setPosition(0);
+                telemetry.addData("Servo:", "0");
+            }
+
+            if(gamepad1.b) {
+                r.servo1.setPosition(1);
+                telemetry.addData("Servo:", "1");
+            }
+
+            if(gamepad1.x) {
+                r.servo1.setPosition(0.6);
+                telemetry.addData("Servo:", "0.6");
+            }
+
+
+            telemetry.addData("rotation LFmotor", r.LFmotor.getPower());
+            telemetry.addData("rotation LBmotor", r.LBmotor.getPower());
+            telemetry.addData("rotation RFmotor", r.RFmotor.getPower());
+            telemetry.addData("rotation RBmotor", r.RBmotor.getPower());
+            telemetry.addData("Degree", degree);
+            telemetry.addData("ServoPos", r.servo1.getPosition());
+            telemetry.update();
+
+
+        }
+    }
+}
